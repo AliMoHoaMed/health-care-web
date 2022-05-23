72cd20091f74b949c5f3f3e4dfe00ce756edd673
AliMoHoaMed <alilomoh16@gmail.com>
2022-05-23 02:13:24 +0200

    changesss

--- src/doccard.js
+++ src/doccard.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './item.css'
-const Doccard = ({id,name,username,avatar}) => {
+const Doccard = ({_id,firstName,Title,avatar,specialtiesId}) => {
   return (
     
     <div>
@@ -10,12 +10,17 @@ const Doccard = ({id,name,username,avatar}) => {
 <div className="cardp" >
 
 
-<img  src={"data:image/jpg;base64," + avatar} width="250" height="250" />
+<img  src={"data:image/jpg;base64,"+avatar} width="250" height="250" />
 </div>    
 <div className="cardinf">
-  <h3 > Dr/name :{name} </h3>
-  <a href={'/doctorpages/'+ id}> info</a>
-
+  <h3 > Dr/name :{firstName} </h3>
+  <a href={'/doctorpages/'+_id}> info</a>
+  <a> 
+    {Title}
+  </a>
+  <a> 
+    {specialtiesId}
+  </a>
 
 <br/>
 
--- src/doctors.js
+++ src/doctors.js
@@ -6,16 +6,38 @@ function Doctors() {
 const [loading ,setloading] =useState(false);
 const [doctors,setposts] = useState([]);
 const [searchtitle,setsearchtitle]=useState('');
-
+const [doctorspec,setdoctorspec] = useState([]);
+const [specc,setspec] = useState([]);
+const [choossestatespec,setchooossestatespec] =useState('');
+const [choose,setchoose] =useState('');
+const [doctitle,setdoctortitle] = useState([]);
 useEffect(()=>{
   const loadposts=async() => {
     setloading(true);
-    const res = await axios.get('https://health-care-app-final.herokuapp.com/doctors')
+    const spe = await axios.get('https://health-care-app-final.herokuapp.com/specialties')
+ setspec(spe.data);
+console.log(spe.data);
+    const res = await axios.get('https://health-care-app-final.herokuapp.com/doctors/search/filter?specialties='+doctorspec +'& drTitle=' + doctitle)
  setposts(res.data);
+ console.log(res.data);
+
+ 
  setloading(false);
   }
   loadposts();
-},[]);
+},[doctorspec,doctitle]);
+
+function handlespec(e) {
+      
+  setdoctorspec(e.target.value);
+
+
+}
+
+function handletitle(e) {
+  setdoctortitle(e.target.value);
+  
+}
 
 
 
@@ -25,16 +47,76 @@ useEffect(()=>{
       <div className='k'> 
       <div className='bb'>
       <input type='text' placeholder='search..' onChange={(e)=> setsearchtitle(e.target.value)}/>
-    <h3>Search</h3></div></div>
+    <h3>Search</h3></div>
+    
+    
+    <select value={choossestatespec}
+  onChange={handlespec} >
+  {
+  specc.map(({ id,specialties})=>(
+<option  key={id} value={id} >{specialties}   </option>
+ ))}</select>
+
+<select value={choose}
+ onChange={(e) => handletitle(e)} >
+ 
+<option value={'Advisor'} >  Advisor </option>
+<option value={'front'}>  front </option>
+<option value={'Advisor'} >  Advisor </option>
+ </select>
+<h2>
+    <a>{choose} </a>
+<a> {doctitle} </a>
+</h2>
+
+    </div>
     {loading ?( <h4> loaaading .....</h4>):( 
 (doctors.filter((value)=>{ 
 if(searchtitle ===''){return doctors}
 else if(value.firstName.toLowerCase().includes(searchtitle.toLocaleLowerCase())){
   return value ;}})
-  .map(({id, firstName , Title,avatar})=>(
-    <div className='grid-container'>
+  .map(({_id,firstName, Title,avatar,specialtiesId})=>(
+    <div className='grid-container' key={_id}>
     <div className='grid-item'>
-   <Doccard id={id} name={firstName} username={Title} avatar={avatar} /></div></div>
+   
+    <div>
+
+<div className="card">
+
+<div className="cardp" >
+
+
+<img  src={"data:image/jpg;base64,"+avatar} width="250" height="250" />
+</div>    
+<div className="cardinf">
+  <h3 > Dr/name :{firstName} </h3>
+  <a href={'/doctorpages/'+_id}> info</a>
+  <a> 
+    {Title}
+  </a>
+  <a> 
+    
+  </a>
+
+<br/>
+
+
+
+
+ <div >
+<table>
+  <tr>
+    <td className="h"> monday 12 pm </td>
+    <br/>
+    <td className="h"> sunday 12 pm </td>
+  
+   
+  </tr>
+</table></div>
+</div>
+</div>
+
+    </div></div></div>
  )) ))} 
     </div>
   );
